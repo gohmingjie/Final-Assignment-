0e409807e2227a5e9f56ab821d9964034f2ab19e
gohmingjie <111936150+gohmingjie@users.noreply.github.com>
2022-08-24 13:32:16 +0800

    Update iris-ml-application.py

--- iris-ml-application.py
+++ iris-ml-application.py
@@ -48,3 +48,16 @@ st.write(iris.target_names[prediction])
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
 
+@st.cache
+ def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
+
+csv = convert_df(my_large_df)
+
+st.download_button(
+     label="Download data as CSV",
+     data=csv,
+     file_name='large_df.csv',
+     mime='text/csv',
+ )
