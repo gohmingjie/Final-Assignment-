4cc4ff9b6a363bba0bd6848956730db76b6c7c35
gohmingjie <111936150+gohmingjie@users.noreply.github.com>
2022-08-24 13:47:51 +0800

    Update iris-ml-application.py

--- iris-ml-application.py
+++ iris-ml-application.py
@@ -48,4 +48,4 @@ st.write(iris.target_names[prediction])
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
 
-st.download_button(label, data, file_name='Iris', mime='text/plain', key=None, help=None, on_click=None, args=None, kwargs=None, *, disabled=False)
+st.download_button(label='Download data as CSV', data=csv, file_name='Iris', mime='text/plain', *, disabled=False)
